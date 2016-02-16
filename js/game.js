ef2e1f145bb023934255aad2aa5543679cc80429
unknown <jbatistm@LIS-CHK3Y52.usersad.everis.int>
2016-02-16 18:09:02 +0000

    Initial commit

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
-var rows = 9;
-var cols = 9;
+var rows = 5, lastRow = 0, minRows = 5, maxRows = Math.floor((window.innerHeight-105-72)/72);;
+var cols = 5, lastCol = 0, minCols = 5, maxCols = Math.floor(window.innerWidth/72);
 var lastRow = 0;
 var lastCol = 0;
 var direction = "up";
@@ -7,16 +7,19 @@ var speed = 1, minspeed = 1, maxspeed = 10;
 var difficulty = 0, mindif = 0, maxdif = 5;
 var runID;
 var score = 0;
+var snakeLength = 0, snakeDistance = 0, maxLength = 2, snakeBody = [];
+var highScore = 0;
 
 document.onkeydown = snakeDirection;
+window.onresize = windowResize;
 
-// Initial Setup
 document.getElementById("rows").innerHTML = rows;
 document.getElementById("cols").innerHTML = cols;
 document.getElementById("speed").innerHTML = speed;
 document.getElementById("difficulty").innerHTML = difficulty;
 
-function snakeDirection(e) {
+function snakeDirection(e)
+{
 
 	e = e || window.event;
 
@@ -33,6 +36,36 @@ function snakeDirection(e) {
 		direction = "right";
 	}
 
+}
+
+function windowResize()
+{
+	// 72x72 is the square size
+	maxRows = Math.floor((window.innerHeight-105-72)/72); //105 is the menu height, and another 72 for the score
+	maxCols = Math.floor(window.innerWidth/72);
+	
+	if(rows>maxRows)
+		rows = maxRows;
+	if(cols>maxCols)
+		cols = maxCols;
+	if(rows<minRows)
+		rows = minRows;
+	if(cols<minCols)
+		cols = minCols;
+	
+	if(runID!=null)
+	{
+		clearInterval(runID);
+		document.getElementById("btn").value = "START";
+		runID=null;
+		if(score>highScore)
+			highScore = score;
+		document.getElementById("highscore").innerHTML = "(High Score: " + highScore + ")";
+	}
+	
+	document.getElementById("rows").innerHTML = rows ;
+	document.getElementById("cols").innerHTML = cols ;
+	drawTable();
 }
 
 function drawTable()
@@ -59,13 +92,14 @@ function drawTable()
 function tableSize(s)
 {
 	if(runID==null){
+				
 		if(s.id=="rowsm" && rows>=6)
 			--rows;
-		else if(s.id=="rowsp" && rows<21)
+		else if(s.id=="rowsp" && rows<maxRows)
 			++rows;
 		else if(s.id=="colsm" && cols>=6)
 			--cols;
-		else if(s.id=="colsp" && cols<21)
+		else if(s.id=="colsp" && cols<maxCols)
 			++cols;
 		else
 			return;
@@ -145,17 +179,30 @@ function play()
 			lastRow=nextRow;
 			lastCol=nextCol;
 			document.getElementById("cell-" + lastRow + "-" + lastCol).style.backgroundColor = "green";
-			score += speed*5 + difficulty*10;
-			document.getElementById("log").innerHTML = "SCORE: " + score;
+			++snakeLength;
+			++snakeDistance;
+			snakeBody.push([lastRow,lastCol]);
+			if(snakeLength>maxLength){
+				document.getElementById("cell-" + snakeBody[0][0] + "-" + snakeBody[0][1]).style.backgroundColor = "black"; 
+				snakeBody.shift();
+				--snakeLength;
+				if(snakeDistance%(maxLength*20)==0)
+					++maxLength;
+			}
+			
+			score += speed*5 + snakeLength*5 + difficulty*10 ;
+			document.getElementById("log").innerHTML = score;
 			return;
 		}
 	
 	document.getElementById("cell-" + lastRow + "-" + lastCol).style.backgroundColor = "red";
-	document.getElementById("log").innerHTML = "SCORE: " + score;
+	document.getElementById("log").innerHTML = score;
 	document.getElementById("btn").value = "START";
 	clearInterval(runID);
 	runID=null;
-	
+	if(score>highScore)
+		highScore = score;
+	document.getElementById("highscore").innerHTML = "(High Score: " + highScore + ")";
 }
 
 function startstop()
@@ -165,6 +212,7 @@ function startstop()
 	{
 		direction = "up";
 		score = 0;
+		snakeLength = 0, snakeDistance = 0, maxLength = 2, snakeBody = [];
 		
 		rows = parseInt(document.getElementById("rows").innerHTML);
 		cols = parseInt(document.getElementById("cols").innerHTML);
@@ -175,7 +223,7 @@ function startstop()
 		clearTable();
 		
 		document.getElementById("btn").value = "STOP";
-		document.getElementById("log").innerHTML = "SCORE: ";
+		document.getElementById("log").innerHTML = "";
 		
 		runID = setInterval(play, 1000/speed);
 	}
@@ -184,6 +232,9 @@ function startstop()
 		document.getElementById("btn").value = "START";
 		clearInterval(runID);
 		runID=null;
+		if(score>highScore)
+			highScore = score;
+		document.getElementById("highscore").innerHTML = "(High Score: " + highScore + ")";
 	}
 	
 	
